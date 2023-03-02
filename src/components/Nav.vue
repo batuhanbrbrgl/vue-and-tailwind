35c9c4eab05d5e38c1d22a6e15a404e44d71f8c0
batuhanbrbrgl <bthnnapcan@gmail.com>
2023-03-02 11:33:59 +0300

    btt

---
+++ src/components/Nav.vue
@@ -0,0 +1,20 @@
+<template>
+  
+<body class="h-screen w-full bg-slate-600 flex">
+<div class="h-16 bg-gray-100 w-full">
+  
+</div>
+</body>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  
+  components: {
+  
+  }
+}
+</script>
